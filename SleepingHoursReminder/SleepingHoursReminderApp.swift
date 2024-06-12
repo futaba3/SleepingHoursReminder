a5b3c83465660a136c7637197a3785dd7602600f
futaba3 <nyokinyoki41aya@gmail.com>
2024-06-12 19:59:47 +0900

    Initial Commit

---
+++ SleepingHoursReminder/SleepingHoursReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepingHoursReminderApp.swift
+//  SleepingHoursReminder
+//
+//  Created by Ayana Kudo on 2024/06/12.
+//
+
+import SwiftUI
+
+@main
+struct SleepingHoursReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
